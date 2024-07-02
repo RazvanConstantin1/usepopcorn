2bb1827849c70247c712523ec515c3430070dcb9
RazvanConstantin1 <razvanconstantin1097@gmail.com>
2024-07-03 01:32:30 +0200

    made another version to practice useEffect, implemented the fetch and the loading state

--- src/index.js
+++ src/index.js
@@ -1,32 +1,25 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
+import "./index.css";
+import App from "./App v1.js";
 
-import StarRating from "./StarRating.js";
+// import StarRating from "./StarRating.js";
 
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
 
-  return (
-    <div>
-      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
-      <p>This movie was rated {movieRating} stars</p>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+//       <p>This movie was rated {movieRating} stars</p>
+//     </div>
+//   );
+// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App />
-     */}
-    <StarRating
-      maxRating={5}
-      messages={["Terrible", "Bad", "Ok", "Good", "Amazing"]}
-    />
-    <StarRating size={24} color="red" className="test" defaultRating={3} />
-    <Test />
+    <App />
   </React.StrictMode>
 );
 
